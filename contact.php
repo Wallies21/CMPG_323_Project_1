eb689d41b5f6b167ec62183f574d8920e23e5519
Wallies21 <waldoackerman@gmail.com>
2020-10-07 11:33:27 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,16 +1,22 @@
 <?php
 
-if (isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
-    $message = $_POST['message'];
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
 
-    $mailTo = "waldoackerman@gmail.com";
+$email_from = 'waldoackerman25@gmail.com';
 
-    $headers = "From: ".$mailFrom;
-    $txt = "You received an email from ".$name. ".\n\n".$message;
+$emai_subject = "New";
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.html?mailsend");
-}
+$emal_body = "User Name: ".$name.".\n\n"."User Email: ".$visitor_email.".\n\n"."User Message: ".$message;
+
+$to = "waldoackerman@gmail.com";
+$headers = "From: ".$email_from."\r\n";
+$headers = "Reply To: ".$visitor_email."\r\n";
+
+mail($to,$emai_subject,$emal_body,$headers);
+
+header("Location: index.html");
+
+
+?>
